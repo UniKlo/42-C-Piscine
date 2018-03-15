bebac679f9322198af0ad5fa01cd02947a44a5a0
UniKlo <33693941+UniKlo@users.noreply.github.com>
2018-03-15 03:02:04 -0700

    Create ft_print_comb.c

---
+++ Day02/ft_print_comb.c
@@ -0,0 +1,49 @@
+#include <unistd.h>
+
+int ft_putchar(char c)
+{
+    write(1, &c, 1);
+    return (0);
+}
+
+void	ft_print_comb(void)
+{
+	char i;
+	char j;
+	char k;
+
+	i = '0';
+	j = '1';
+	k = '2';
+	
+	
+	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+			while (k <= '9')
+			{
+				if (i != j && j != k && i != k)
+				{
+					ft_putchar(i);
+					ft_putchar(j);
+					ft_putchar(k);
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				k++;
+			}
+			j++;
+			k = j;
+		}
+		i++;
+		j = i;
+	}
+	
+
+}
+int main(void)
+{
+    ft_print_comb();    
+    return 0;
+}
