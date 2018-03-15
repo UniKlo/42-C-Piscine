0d5be854ed4aa6127ace6b5ff049c6e32caa896a
UniKlo <33693941+UniKlo@users.noreply.github.com>
2018-03-15 01:36:24 -0700

    Update ft_is_negative.c

--- Day02/ft_is_negative.c
+++ Day02/ft_is_negative.c
@@ -1,1 +1,29 @@
 #include <unistd.h>
+
+int ft_putchar(char c)
+
+{
+    write (1, &c, 1);
+    return (0);
+}
+
+int ft_is_negative(int n)
+{
+    if (n <0)
+    {
+        ft_putchar('N');
+    }
+    else
+    {
+        ft_putchar('P');
+    }
+    
+    return (0);
+}
+
+int main(void)
+
+{
+    ft_is_negative(6);
+    return (0);
+}
