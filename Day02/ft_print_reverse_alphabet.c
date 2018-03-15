18a8154136b3369cdffa880c687d92332ba88bf8
UniKlo <33693941+UniKlo@users.noreply.github.com>
2018-03-15 00:06:14 -0700

    Update ft_print_reverse_alphabet.c

--- Day02/ft_print_reverse_alphabet.c
+++ Day02/ft_print_reverse_alphabet.c
@@ -1,1 +1,20 @@
+#include <unistd.h>
 
+void ft_print_alphabet(void)
+{
+    char c='z';
+    while (c >= 'a')
+    {
+        write(1, &c, 1);
+        c--;
+    }
+
+}
+
+int main (void)
+
+{
+    ft_print_alphabet();
+    return (0);
+    
+}
